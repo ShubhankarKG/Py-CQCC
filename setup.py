71d99b7588542c6bdd8df53522cbb7e4862d7ae5
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2022-01-15 20:48:14 +0530

    Add necessary files for upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Py-CQCC",
+    version="0.0.1",
+    author="Shubhankar Gupta",
+    author_email="shubhankar.gupto.11@gmail.com",
+    description="Python implementation of CQCC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShubhankarKG/Py-CQCC",
+    project_urls={
+        "Bug Tracker": "https://github.com/ShubhankarKG/Py-CQCC/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
