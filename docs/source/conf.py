1b3db1767b85218737935a65516ad5c011f44f15
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2022-01-17 12:54:09 +0530

    refactor: add index as master_doc in docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Shubhankar Gupta'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
